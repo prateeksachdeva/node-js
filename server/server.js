ca16651814653cc0487a84ee74a0b68b275fc090
prateeksachdeva <june24.prateek@gmail.com>
2017-09-29 16:37:51 +0530

    bro

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+var {ObjectID}=require('mongodb');
 
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./model/todo');
@@ -22,6 +23,20 @@ Todo.find().then((todos)=>{
 },(e)=>{
   res.status(400).send(e);
 }) ;
+});
+app.get('/todos/:id',(req,res)=>{
+var id =req.params.id;
+if (!ObjectID.isValid(id)){
+  return res.status(404).send();
+}
+Todo.findById(id).then((resu)=>{
+  if (!resu){
+  return  res.status(404).send();
+  }
+  res.send({resu});
+}).catch((e)=>{
+  res.status(400).send();
+});
 });
 app.listen(300,()=>{
     console.log('Started on port 300');
